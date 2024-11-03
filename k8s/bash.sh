399be82bb5e73e4888909f4fd409db46896b3f14
Guilherme Ferreira <guilherme.fsilva23@hotmail.com>
2024-11-03 19:01:49 -0300

    chore: adding deployment kubernetes, loadbalancer, replicaset

---
+++ k8s/bash.sh
@@ -0,0 +1,5 @@
+# Create pod
+kubectl apply -f ./k8s/deployments/deploy.yaml
+
+# Create service
+kubectl apply -f ./k8s/services/service.yaml
---
+++ k8s/delete.sh
@@ -0,0 +1,5 @@
+# Delete deployments
+kubectl delete -f ./k8s/deployments/deploy.yaml
+
+# Delete services
+kubectl delete -f ./k8s/services/service.yaml
